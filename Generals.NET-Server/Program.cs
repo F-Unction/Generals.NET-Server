9cb1e9cae011841e623e6b16a203110261cee0b8
Unction F <2148765298@qq.com>
2020-08-15 14:32:37 +0800

    Created the solution

---
+++ Generals.NET-Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generals.NET_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
